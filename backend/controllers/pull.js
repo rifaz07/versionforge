c5f3bfaf2ab74ff77b8206ec0edecfaadc65465a
rifaz shaikh razak <rifazshaikh07@gmail.com>
2026-03-30 15:30:17 +0530

    feat(pull): implement pullRepo to download commits from AWS S3

--- backend/controllers/pull.js
+++ backend/controllers/pull.js
@@ -3,7 +3,7 @@ const path = require("path");
 const { s3, S3_BUCKET } = require("../config/aws-config");
 
 async function pullRepo() {
-  const repoPath = path.resolve(process.cwd(), ".apnaGit");
+  const repoPath = path.resolve(process.cwd(), ".versionforge");
   const commitsPath = path.join(repoPath, "commits");
 
   try {
