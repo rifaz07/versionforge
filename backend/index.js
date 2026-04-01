a38eb49a75be92064477470dbdd1cb44027df392
rifaz shaikh razak <rifazshaikh07@gmail.com>
2026-04-01 21:47:43 +0530

    backend: setup express server, mongodb connection and socket.io base

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,11 @@
-#!/usr/bin/env node
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const http = require("http");
+const { Server } = require("socket.io");
+// const mainRouter = require("./routes/main.router");
 
 const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
@@ -10,70 +17,100 @@ const { pushRepo } = require("./controllers/push");
 const { pullRepo } = require("./controllers/pull");
 const { revertRepo } = require("./controllers/revert");
 
+dotenv.config();
+
 yargs(hideBin(process.argv))
-.command("start", "starts a anew server", {}, () => {
-    startServer();
-})
-
-.command("init", "Initialize repository", {}, () => {
-    initRepo();
-})
-
-.command(
+  .command("start", "Starts a new server", {}, startServer)
+  .command("init", "Initialise a new repository", {}, initRepo)
+  .command(
     "add <file>",
-    "Add file to staging",
+    "Add a file to the repository",
     (yargs) => {
-        yargs.positional("file", {
-            describe: "File to stage",
-            type: "string"
-        });
+      yargs.positional("file", {
+        describe: "File to add to the staging area",
+        type: "string",
+      });
     },
     (argv) => {
-        addRepo(argv.file);
+      addRepo(argv.file);
     }
-)
-
-.command(
+  )
+  .command(
     "commit <message>",
-    "Commit staged files",
+    "Commit the staged files",
     (yargs) => {
-        yargs.positional("message", {
-            describe: "Commit message",
-            type: "string"
-        });
+      yargs.positional("message", {
+        describe: "Commit message",
+        type: "string",
+      });
     },
     (argv) => {
-        commitRepo(argv.message);
+      commitRepo(argv.message);
     }
-)
-
-.command("push", "Push commits", {}, () => {
-    pushRepo();
-})
-
-.command("pull", "Pull commits", {}, () => {
-    pullRepo();
-})
-
-.command(
+  )
+  .command("push", "Push commits to S3", {}, pushRepo)
+  .command("pull", "Pull commits from S3", {}, pullRepo)
+  .command(
     "revert <commitID>",
-    "Revert commit",
+    "Revert to a specific commit",
     (yargs) => {
-        yargs.positional("commitID", {
-            describe: "Commit ID",
-            type: "string"
-        });
+      yargs.positional("commitID", {
+        describe: "Comit ID to revert to",
+        type: "string",
+      });
     },
     (argv) => {
-        revertRepo(argv.commitID);
+      revertRepo(argv.commitID);
     }
-)
-
-.demandCommand(1, "You must provide a command")
-.help()
-.argv;
-
+  )
+  .demandCommand(1, "You need at least one command")
+  .help().argv;
 
 function startServer() {
-    console.log("Starting VersionForge server...");
+  const app = express();
+  const port = process.env.PORT || 3000;
+
+  app.use(bodyParser.json());
+  app.use(express.json());
+
+  const mongoURI = process.env.MONGODB_URI;
+
+  mongoose
+    .connect(mongoURI)
+    .then(() => console.log("MongoDB connected!"))
+    .catch((err) => console.error("Unable to connect : ", err));
+
+  app.use(cors({ origin: "*" }));
+
+   
+
+  let user = "test";
+  const httpServer = http.createServer(app);
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    socket.on("joinRoom", (userID) => {
+      user = userID;
+      console.log("=====");
+      console.log(user);
+      console.log("=====");
+      socket.join(userID);
+    });
+  });
+
+  const db = mongoose.connection;
+
+  db.once("open", async () => {
+    console.log("CRUD operations called");
+    // CRUD operations
+  });
+
+  httpServer.listen(port, () => {
+    console.log(`Server is running on PORT ${port}`);
+  });
 }
