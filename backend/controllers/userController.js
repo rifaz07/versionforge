b2427a7b66b52a5d243863b39d9200a1aa3b2818
rifaz shaikh razak <rifazshaikh07@gmail.com>
2026-04-02 22:22:56 +0530

    feat(user): complete user CRUD (signup, login, profile, update, delete) with MongoDB and JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,28 +1,206 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs"); 
+const {MongoClient} = require("mongodb"); 
+// const dotenv = require("dotenv");
+// dotenv.config();
+// const uri = process.env.MONGODB_URI;
+const { ObjectId } = require("mongodb");
 
-const signup = (req, res) => {
- res.send("Signup");
-};
 
-const login = (req, res) => {
-  res.send("Login");
-};
+let client;
 
-const getAllUsers = (req, res) => {
-  res.send("Get all users");
-};
+async function connectClient() {
+  if (!client) {
+    const uri = process.env.MONGODB_URI;
 
-const getUserProfile = (req, res) => {
-  res.send("Get user profile");
-};
+    if (!uri) {
+      throw new Error("MONGODB_URI is not defined in .env");
+    }
 
-const updateUserProfile = (req, res) => {
- res.send("Update user profile");
-};
+    client = new MongoClient(uri);
+    await client.connect();
+  }
+}
+// console.log("JWT:", process.env.JWT_SECRET_KEY);
 
-const deleteUserProfile = (req, res) => {
-    res.send("Delete user profile");
-};
 
+async function signup(req, res) {
+  console.log("FULL BODY:", req.body); 
+  const { username, password, email } = req.body;
+  try {
+    await connectClient();
+    const db = client.db("versionforge");
+    const usersCollection = db.collection("users");
+
+    const user = await usersCollection.findOne({ username });
+    if (user) {
+      return res.status(400).json({ message: "User already exists!" });
+    }
+    // console.log("BODY:", req.body);
+    // console.log("PASSWORD:", password);
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    // console.log("HASHED PASSWORD:", hashedPassword);
+    const newUser = {
+      username,
+      password: hashedPassword,
+      email,
+      repositories: [],
+      followedUsers: [],
+      starRepos: [],
+    };
+
+    const result = await usersCollection.insertOne(newUser);
+
+    const token = jwt.sign(
+      { id: result.insertedId },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+    res.json({ token, userId: result.insertedId });
+  } catch (err) {
+    console.error("Error during signup : ", err.message);
+    res.status(500).send("Server error");
+  }
+}
+
+async function login(req, res) {
+  const { email, password } = req.body;
+  try {
+    await connectClient();
+    const db = client.db("versionforge");
+    const usersCollection = db.collection("users");
+
+    const user = await usersCollection.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials!" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials!" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1h",
+    });
+    res.json({ token, userId: user._id });
+  } catch (err) {
+    console.error("Error during login : ", err.message);
+    res.status(500).send("Server error!");
+  }
+}
+
+
+async function getAllUsers(req, res) {
+  try {
+    await connectClient();
+    const db = client.db("versionforge");
+    const usersCollection = db.collection("users");
+
+    const users = await usersCollection
+      .find({}, { projection: { password: 0 } })
+      .toArray();
+
+    res.json(users);
+  } catch (err) {
+    console.error("Error during fetching : ", err.message);
+    res.status(500).send("Server error!");
+  }
+}
+
+
+async function getUserProfile(req, res) {
+  const currentID = req.params.id;
+
+  try {
+    await connectClient();
+    const db = client.db("versionforge");
+    const usersCollection = db.collection("users");
+
+    const user = await usersCollection.findOne(
+      { _id: new  ObjectId(currentID) },
+      { projection: { password: 0 } }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+
+    res.json(user);
+
+  } catch (err) {
+    console.error("Error during fetching : ", err.message);
+    res.status(500).send("Server error!");
+  }
+}
+
+
+async function  updateUserProfile(req, res) {
+  const currentID = req.params.id;
+  const { email, password } = req.body;
+
+  try {
+    await connectClient();
+    const db = client.db("versionforge");
+    const usersCollection = db.collection("users");
+
+    let updateFields = {};
+
+    if (email) {
+      updateFields.email = email;
+    }
+
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      updateFields.password = hashedPassword;
+    }
+
+    const result = await usersCollection.findOneAndUpdate(
+      { _id: new ObjectId(currentID) },
+      { $set: updateFields },
+      { returnDocument: "after", projection: { password: 0 } }
+    );
+
+    if (!result.value) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+
+    res.json(result.value);
+
+  } catch (err) {
+    console.error("Error during updating : ", err.message);
+    res.status(500).send("Server error!");
+  }
+}
+
+
+
+async function deleteUserProfile(req, res) {
+  const currentID = req.params.id;
+
+  try {
+    await connectClient();
+    const db = client.db("versionforge");
+    const usersCollection = db.collection("users");
+
+    const result = await usersCollection.deleteOne({
+      _id: new ObjectId(currentID),
+    });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+
+    res.json({ message: "User Profile Deleted!" });
+
+  } catch (err) {
+    console.error("Error during deleting : ", err.message);
+    res.status(500).send("Server error!");
+  }
+}
 module.exports = {
   getAllUsers,
   signup,
@@ -31,3 +209,4 @@ module.exports = {
   updateUserProfile,
   deleteUserProfile,
 };
+
